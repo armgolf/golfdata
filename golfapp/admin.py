b9a46f5632311ae1f7f0a080c0d8c0af45562811
armgolf <armitchell@hotmail.co.uk>
2017-03-01 21:27:33 +0000

    Mods to admin & urls.

--- golfapp/admin.py
+++ golfapp/admin.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
-from .models import ShotPercentages
+from .models import ShotPercentages, Golfscore, TotalScores
 
 class ShotPercentagesAdmin(admin.ModelAdmin):
     pass
 
 admin.site.register(ShotPercentages, ShotPercentagesAdmin)
+
+class GolfscoreAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Golfscore, GolfscoreAdmin)
+
+class TotalScoresAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(TotalScores, TotalScoresAdmin)
--- golfdata/urls.py
+++ golfdata/urls.py
@@ -12,8 +12,8 @@ urlpatterns = [
             success_url='/'
     ), name='register'),
     url('^accounts/', include('django.contrib.auth.urls')),
-    url(r'^$', views.display, name='display'),
-    url(r'^homepage/$', views.homepage, name='homepage'),
+    url(r'^$', views.homepage, name='homepage'),
+    url(r'^dashboard/$', views.display, name='display'),
     url(r'^golfscore/$', views.golfscore_entry, name='golfscore_entry'),
     url(r'^golfscore/(?P<pk>\d+)/$', views.golfscore_detail, name='golfscore_detail'),
     url(r'^golfscore/(?P<pk>\d+)/edit/$', views.golfscore_edit, name='golfscore_edit'),
